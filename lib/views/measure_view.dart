8d5b70123dd92147b8255aa87a41aee1e2f1389c
Szántó Benjámin <szantobenjamin97@gmail.com>
2024-01-25 17:27:38 +0100

    Update measure_view.dart

--- lib/views/measure_view.dart
+++ lib/views/measure_view.dart
@@ -7,38 +7,43 @@ class MeasureView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Measure", style: TextStyle(fontSize: 40)),
-            SizedBox(height: 10),
-            Text("Core", style: TextStyle(fontSize: 18, color: Color(0xFFCCCCCC))),
-            SizedBox(height: 5),
-            TextButton(onPressed: () {controller.navToWeightView();}, child: Text("Weight", style: TextStyle(fontSize: 25, color: Colors.white),)),
-            SizedBox(height: 5),
-            Text(
-              controller.weightController.text,
-              style: TextStyle(fontSize: 25, color: Color(0xFFCCCCCC)),
+    return GetBuilder<MeasureViewController>(
+      init: controller,
+      builder(_){
+        return Scaffold(
+          appBar: AppBar(),
+          body: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Measure", style: TextStyle(fontSize: 40)),
+                SizedBox(height: 10),
+                Text("Core", style: TextStyle(fontSize: 18, color: Color(0xFFCCCCCC))),
+                SizedBox(height: 5),
+                TextButton(onPressed: () {controller.navToWeightView();}, child: Text("Weight", style: TextStyle(fontSize: 25, color: Colors.white),)),
+                SizedBox(height: 5),
+                Text(
+                  controller.weightController.text,
+                  style: TextStyle(fontSize: 25, color: Color(0xFFCCCCCC)),
+                ),
+                SizedBox(height: 5),
+                TextButton(onPressed: () {controller.navToBodyFatPercentageView();}, child: Text("Body fat percentage", style: TextStyle(fontSize: 25, color: Colors.white),)),
+                Text(
+                  controller.bodyFatPercentageController.text,
+                  style: TextStyle(fontSize: 25, color: Color(0xFFCCCCCC)),
+                ),
+                SizedBox(height: 5),
+                TextButton(onPressed: () {controller.navToCaloricIntakeView();}, child: Text("Caloric intake", style: TextStyle(fontSize: 25, color: Colors.white),)),
+                Text(
+                  controller.caloricIntakeController.text,
+                  style: TextStyle(fontSize: 25, color: Color(0xFFCCCCCC)),
+                ),
+              ],
             ),
-            SizedBox(height: 5),
-            TextButton(onPressed: () {controller.navToBodyFatPercentageView();}, child: Text("Body fat percentage", style: TextStyle(fontSize: 25, color: Colors.white),)),
-            Text(
-              controller.bodyFatPercentageController.text,
-              style: TextStyle(fontSize: 25, color: Color(0xFFCCCCCC)),
-            ),
-            SizedBox(height: 5),
-            TextButton(onPressed: () {controller.navToCaloricIntakeView();}, child: Text("Caloric intake", style: TextStyle(fontSize: 25, color: Colors.white),)),
-            Text(
-              controller.caloricIntakeController.text,
-              style: TextStyle(fontSize: 25, color: Color(0xFFCCCCCC)),
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      }
     );
   }
 }
